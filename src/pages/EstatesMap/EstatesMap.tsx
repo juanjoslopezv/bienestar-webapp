77cad68de5eb8256a9c0e3d464abc8a961f23500
Juan Jose Lopez <juanjoslopezv@gmail.com>
2022-03-10 23:59:11 -0600

    Fixed issue on mobile and recentered

--- src/pages/EstatesMap/EstatesMap.tsx
+++ src/pages/EstatesMap/EstatesMap.tsx
@@ -14,25 +14,29 @@ const EstatesMap = () => {
 
   const mapDefaults = {
     center: {
-      lat: 19.2400444,
-      lng: -103.7636272
+      lat: 19.247410,
+      lng: -103.720139
     },
     zoom: 13
   }
 
   const Estates = estatesCtx.Estates?.data || []
 
-  return <Box flex={1} display="flex">
+  return <Box flex={1} display="flex" w={{
+    base: '100vw',
+    md: 'auto'
+  }} h={"calc(100vh - 60px)"}>
 
     <GoogleMapReact
       bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY! }}
       defaultCenter={mapDefaults.center}
       defaultZoom={mapDefaults.zoom}
     >
-      {Estates.map((estate) => {
+      {Estates.map((estate, key) => {
         return <EstateMarker
+          key={key}
           lat={estate.attributes.latitude}
-          lng={estate.attributes.longitude} 
+          lng={estate.attributes.longitude}
           estate={estate} />
       })}
     </GoogleMapReact>
